bb8b4dee31eaa82b9b12cccbcd42f581fc9a6e66
tariktabakovic <tariktab97@gmail.com>
2020-06-13 16:28:29 -0400

    create function for getting the date

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ const api= {
 }
 
 function App() {
+
+  const dateBuilder = (d) =>{
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+    'Nevember', 'December'];
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
   return (
     <div className="app">
       <main>
@@ -17,7 +30,7 @@ function App() {
         <div>
           <div className="location-box">
             <div className="location">New York City, US</div>
-            {/* <div className="date">{dateBuilder(new date())}</div> */}
+            <div className="date">{dateBuilder(new Date())}</div>
           </div>
         </div>
       </main>
