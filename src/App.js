de3aba9d73eb3edb173fbea961da8fb39050c465
tariktabakovic <tariktab97@gmail.com>
2020-06-13 15:45:52 -0400

    creates search box

--- src/App.js
+++ src/App.js
@@ -7,7 +7,14 @@ const api= {
 function App() {
   return (
     <div className="App">
-
+      <main>
+        <div className="search-box">
+          <input 
+            type="text" 
+            className="search-bar" 
+            placeholder="Search..."></input>
+        </div>
+      </main>
     </div>
   );
 }
