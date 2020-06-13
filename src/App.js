24b43123b6ef282ae62e0b5dcee1dc38c54383e5
tariktabakovic <tariktab97@gmail.com>
2020-06-13 16:40:11 -0400

    creates search function to fetch weather from api

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
 
+  const search = evt => {
+    if(evt.key === "Enter"){
+      fetch(`$(api.base)weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => setWeather(result))
+    }
+  }
 
   const dateBuilder = (d) =>{
     let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
