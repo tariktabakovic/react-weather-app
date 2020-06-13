2a7e9784f55da5dfc3926a3f94c0369ccd84f9b5
tariktabakovic <tariktab97@gmail.com>
2020-06-13 16:37:52 -0400

    creates constants for setting query and weather

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import useState from 'react';
 const api= {
   key: '4b3f25e782223fd3ef9a2cc314e68a54',
   base: 'https://api.openweathermap.org/data/2.5/'
 }
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
 
   const dateBuilder = (d) =>{
     let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
